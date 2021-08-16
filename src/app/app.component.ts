0ca2f51206e7baac0af30199d7777560d0c55fba
AntoRin_ux <antorinish@gmail.com>
2021-08-16 15:18:11 +0530

    add special char regex, fix styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,63 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Users } from './models/user.interface';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Users } from "./models/user.interface";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   selector: "app-root",
+   templateUrl: "./app.component.html",
+   styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-  title(title: any) {
-    throw new Error("Method not implemented.");
-  }
-  emailPattern:string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
-  mobPattern:string = "^[0-9]{10}$";
-  formGroup: FormGroup = new FormGroup({});
-  public users: Users[] = [];
-  public showErrors: boolean = false;
+   public emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$";
+   public mobPattern: string = "^[0-9]{10}$";
+   public specialCharPattern: RegExp = new RegExp(/^[^*|\":<>[\]{}`\\()';@&$]+$/);
+   public formGroup: FormGroup = new FormGroup({});
+   public users: Users[] = [];
+   public showErrors: boolean = false;
 
-  constructor(private fb: FormBuilder) {
- 
-  }
+   constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
-    this.formGroup = this.fb.group({
-      firstName: ['', [Validators.required, Validators.maxLength(50)]],
-      lastName: ['', [Validators.required]],
-      email:['',[Validators.required, Validators.pattern(this.emailPattern)]],
-      contact: ['', [Validators.required]],
-      address: this.fb.group({
-        street: ['', [Validators.required]],
-        city: ['', [Validators.required]],
-        state: ['', [Validators.required]],
-        zip: ['', [Validators.required, Validators.pattern(this.mobPattern)]]
-      }),
-    });
-  }  
+   ngOnInit(): void {
+      this.formGroup = this.fb.group({
+         firstName: ["", [Validators.required, Validators.maxLength(50), Validators.pattern(this.specialCharPattern)]],
+         lastName: ["", [Validators.required, Validators.pattern(this.specialCharPattern)]],
+         email: ["", [Validators.required, Validators.pattern(this.emailPattern)]],
+         contact: ["", [Validators.required, Validators.pattern(this.mobPattern), Validators.pattern(this.specialCharPattern)]],
+         address: this.fb.group({
+            street: ["", [Validators.required, Validators.pattern(this.specialCharPattern)]],
+            city: ["", [Validators.required, Validators.pattern(this.specialCharPattern)]],
+            state: ["", [Validators.required, Validators.pattern(this.specialCharPattern)]],
+            zip: ["", [Validators.required, Validators.pattern(this.specialCharPattern)]],
+         }),
+      });
+   }
 
-  onSubmit(): void {
-    const { firstName, lastName, email, contact } = this.formGroup.value;
-    const { street, city, state, zip } = this.formGroup.get("address")?.value;
+   onSubmit(): void {
+      const { firstName, lastName, email, contact } = this.formGroup.value;
+      const { street, city, state, zip } = this.formGroup.get("address")?.value;
 
-    
-    const user: Users = {
-      firstName,
-      lastName,
-      email,
-      contact,
-      address: {
-        street,
-        city,
-        state,
-        zip
-      }
-    }
-    
-    this.showErrors = true;
+      const user: Users = {
+         firstName,
+         lastName,
+         email,
+         contact,
+         address: {
+            street,
+            city,
+            state,
+            zip,
+         },
+      };
 
-    if (this.formGroup.valid)
-      this.users.push(user);
+      this.showErrors = true;
 
-  }
+      if (this.formGroup.valid) this.users.push(user);
+   }
 }
